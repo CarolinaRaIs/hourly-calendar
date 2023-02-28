478f559bc9574c07cebbe39b3865c464fafbd5fb
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-02-27 22:25:54 -0800

    Debugged live timer to display current day and time in header at an interval of every 1000 ms = 1s

--- script.js
+++ script.js
@@ -18,13 +18,14 @@ $(document).ready(function () {
       // (3A) We can use 'localStorage.setItem' to save the description in local storage. The id is useful because that is what is used to reference the key in local storage. 
   
   $('.saveBtn').on('click', function () {
-    console.log(this)
+    console.log(this);
     console.log("hi")
     // (2Q)
     // this.closest('.time-block') --> "Select the closest ancestor element of the saveBtn (this) that has a class of time-block"
     // (closest ancestor element (ie:<div>) of the saveBtn with class time-block).attr('id') ---> get the id attribute of the closest ancestor element of the saveBtn
     // "Get the id of the time-block container (ie: id = "hour-x") that contains the save button that was clicked (this).
     var timeBlockId = $(this).closest('.time-block').attr('id');
+    console.log(timeBlockId);
     // ".querySelector('.description') for js ---> .find('.description') for jquery---> "Select the first element with a class of description that is a descendant of the time-block element returned by this.closest('.time-block'). "
     // (<textarea>).val() ---> What the user types (input)
     // "Get the user input from the textarea element"
@@ -37,11 +38,19 @@ $(document).ready(function () {
 
 
   // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // block by comparing the id to the current hour. 
+  // PROCESS: 
+  // How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
   //
+
+  // for(var i =0; i<24; i++) {
+  //   if(('#hour-' + i) < current)
+  // }
+  // function subjectiveTime() {
+
+  // }
+
+
 
 
 
@@ -55,23 +64,37 @@ $(document).ready(function () {
   // Retrieve saved data (input text = "referenced by timeBlockId thats name "hour-x") from local storage 
   // ('#hour-x .description) -----> selects the textarea element with a class of .description and an id of hour-x. The <textarea> element is a descendent of the <div> element (has the id of hour-x).
   // (localStorage.getItem("id attribute of each time-block")) --> gets the value stored in local storage under the key "hour-x".
-  $('#hour-6 .description').val(localStorage.getItem('hour-6'));
-  $('#hour-7 .description').val(localStorage.getItem('hour-7'));
-  $('#hour-8 .description').val(localStorage.getItem('hour-8'));
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  $('#hour-18 .description').val(localStorage.getItem('hour-18'));
+  
+  // Use iteration (for loop) to simplify code:
+      // ${} ---> Use of string interpolation to embed variable i. i will be converted to a string
+      // $(`#hour-${i} .description`).val(localStorage.getItem('hour-${i}'));
+      // Using back ticks (``) not single quotes ('') to interpolate the i value of the string
+        // $('#hour-6 .description').val(localStorage.getItem('hour-6')); .......
+        // $('#hour-18 .description').val(localStorage.getItem('hour-18')); .........
+        
+for (var i=6; i <= 18; i++) {
+  $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`));
+}
 
+  
 
   // TODO: Add code to display the current date in the header of the page.
 
-    // moment.js to access live clock
-    // $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    // day.js to access live clock
+    // day() = a function in day.js that repressents current date and time
+    // format() = takes string argument for format that want to use
+    // "MMMM Do YYYY, h:mm:ss a" = "Month Day Year, Hour:Minute:Second AM/PM".
+    // link day.js in html for this code to work
+
+var currentDayEl = $('#currentDay');
+
+function displayCurrentTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  currentDayEl.text(rightNow);
+  console.log(displayCurrentTime);
+}
+
+displayCurrentTime();
+setInterval(displayCurrentTime, 1000);
+
 });
