15e6b6eb995848bfbde22652a96aab4e95db0171
CarolinaRaIs <115561193+CarolinaRaIs@users.noreply.github.com>
2023-02-28 11:37:23 -0800

    Added code to script.js to apply past, present, or future classes to each timeblock by comparing the id to the current hour

--- script.js
+++ script.js
@@ -40,15 +40,34 @@ $(document).ready(function () {
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. 
   // PROCESS: 
-  // How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-  //
+  // (1Q) How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? 
+      // (1A) The id attribute of each time-block (hour-i) can be used to compare its value to the current time.
+  // (2Q) How can Day.js be used to get the current hour in 24-hour time?
+      // (2A) dayjs().hour() ---> gets current hour in 24-hour time format
+  
+  //(2A)
+  var currentTime = dayjs().hour()
+  
+  // (1A)
+  // Instead of checking each time block (#hour-i) independently, iterate conditional with a for loop (i starts at 6 AM, ends at 6PM = (i=18))
+  for(var i =6; i<=18; i++) {
+    // `` ---> creates a template literal so that can use dynamic value (i) into the string
+    var timeBlockId = $(`#hour-${i}`);
 
-  // for(var i =0; i<24; i++) {
-  //   if(('#hour-' + i) < current)
-  // }
-  // function subjectiveTime() {
-
-  // }
+    if (timeBlockId < currentTime) {
+    // .addClass ---> jQuery function used to add class "past" (change styling) to an element
+    timeBlockId.addClass("past");
+    } else if (i === currentTime) {
+    // .removeClass ---> jQuery function used to remove class "past" (change styling) to an element
+    timeBlockId.removeClass("past");
+    timeBlockId.addClass("present");
+    } else {
+    timeBlockId.removeClass("past");
+    timeBlockId.removeClass("present");
+    timeBlockId.addClass("future");
+    }
+  }
+  
 
 
 
@@ -73,10 +92,10 @@ $(document).ready(function () {
         // $('#hour-6 .description').val(localStorage.getItem('hour-6')); .......
         // $('#hour-18 .description').val(localStorage.getItem('hour-18')); .........
         // Can confirm that task was succesfully achieved because in "Application" tab in Devtools, you can see that key "hour-i" (key) is associated to the input typed in (value) in the local storage
-for (var i=6; i <= 18; i++) {
-  userInput = $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`));
-  console.log(userInput);
-}
+  for (var i=6; i <= 18; i++) {
+    userInput = $(`#hour-${i} .description`).val(localStorage.getItem(`hour-${i}`));
+    console.log(userInput);
+  }
 
   // TODO: Add code to display the current date in the header of the page.
 
@@ -86,16 +105,16 @@ for (var i=6; i <= 18; i++) {
     // "MMMM Do YYYY, h:mm:ss a" = "Month Day Year, Hour:Minute:Second AM/PM".
     // link day.js in html for this code to work
 
-var currentDayEl = $('#currentDay');
+  var currentDayEl = $('#currentDay');
 
-function displayCurrentTime() {
-  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-  currentDayEl.text(rightNow);
-  console.log(displayCurrentTime);
-}
+  function displayCurrentTime() {
+    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    currentDayEl.text(rightNow);
+    console.log(displayCurrentTime);
+  }
 
-displayCurrentTime();
-// Set interval to have time continually update instead of only when refresh the page
-setInterval(displayCurrentTime, 1000);
+  displayCurrentTime();
+  // Set interval to have time continually update instead of only when refresh the page
+  setInterval(displayCurrentTime, 1000);
 
 });
